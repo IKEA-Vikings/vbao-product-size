bae781622d3a4567a1968717d066aa48faec3a48
vbaobao <vbao.main@gmail.com>
2021-03-03 19:44:18 -0800

    Write db connection and basic set/get functions

---
+++ server/database.js
@@ -0,0 +1,43 @@
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const { Schema } = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/ikea', {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.on('error', console.error.bind(console, 'connection error: '));
+mongoose.connection.once('open', () => console.log('Connection successful!'));
+
+const singleSizeSchema = new Schema({
+  name: String,
+  size: String,
+  unit: String
+});
+
+const productSizesSchema = new Schema({
+  sizes: {
+    type: [singleSizeSchema],
+    default: undefined
+  }
+});
+
+const singleSize = mongoose.model('singleSize', singleSizeSchema);
+const productSizes = mongoose.model('productSizes', productSizesSchema);
+
+function setProductSizes(documents, callback) => {
+  productSizes.insertMany(documents, (err, docs) => {
+    if (err) { return callback(err);}
+    callback(null, docs);
+  });
+}
+
+function getProductSizes(documents, callback) => {
+  productSizes.find({}, (err, docs) => {
+    if (err) { return callback(err);}
+    callback(null, docs);
+  });
+}
+
+module.exports = {
+  setProductSizes,
+  getProductSizes
+};
