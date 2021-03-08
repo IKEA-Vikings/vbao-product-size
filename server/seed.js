9390113d5070d9025e10ecf245c0f89bb178c821
vbaobao <vbao.main@gmail.com>
2021-03-07 18:37:06 -0800

    put data generation into a function

--- server/seed.js
+++ server/seed.js
@@ -4,40 +4,45 @@ const seed = Promise.promisify(db.setProductSizes);
 const ProductSizes = db.productSizes;
 
 /* Data Generation */
-let data = [];
-const random = (max) => Math.floor(Math.random() * Math.floor(max));
-const sizeType = [
-  ['length', 'in'],
-  ['width', 'in'],
-  ['height', 'in'],
-  ['Thread count', '/in^2'],
-  ['Filling weight', 'oz'],
-  ['Total weight', 'oz'],
-  ['Max. load', 'lb']
-];
+const generateData = () => {
+  let data = [];
+  const random = (max) => Math.floor(Math.random() * Math.floor(max));
+  const sizeType = [
+    ['length', 'in'],
+    ['width', 'in'],
+    ['height', 'in'],
+    ['Thread count', '/in^2'],
+    ['Filling weight', 'oz'],
+    ['Total weight', 'oz'],
+    ['Max. load', 'lb']
+  ];
 
-for (let i = 0; i < 100; i++) {
-  let document = { sizes: [] };
-  let numSizes = random(8);
+  for (let i = 0; i < 100; i++) {
+    let document = { sizes: [] };
+    let numSizes = random(8);
 
-  for (let j = 0; j < numSizes; j++) {
-    let temp = {};
-    let index = random(4);
-    //Pick random number
-    temp['name'] = sizeType[index][0];
-    //Add name and unit to temp
-    temp['unit'] = sizeType[index][1];
-    //Pick random size, add to temp
-    temp['size'] = random(75);
-    //Push temp to document[sizes];
-    document.sizes.push(temp);
+    for (let j = 0; j < numSizes; j++) {
+      let temp = {};
+      let index = random(4);
+      //Pick random number
+      temp['name'] = sizeType[index][0];
+      //Add name and unit to temp
+      temp['unit'] = sizeType[index][1];
+      //Pick random size, add to temp
+      temp['size'] = random(75);
+      //Push temp to document[sizes];
+      document.sizes.push(temp);
+    }
+    //push document to data
+    data.push(document);
   }
-  //push document to data
-  data.push(document);
-}
+
+  return data;
+};
 
 /* Data Insertion */
 ProductSizes.deleteMany();
+let data = generateData();
 seed(data)
   .then((results) => {
     console.log(results);
