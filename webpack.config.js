6947dc7ec72d99f7816aebd79bc5443123d05aea
vbaobao <vbao.main@gmail.com>
2021-03-08 00:01:05 -0800

    Update entry filename

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './client/App.js',
+  entry: './client/App.jsx',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
