f3a9173b76becc9930b8aeee795817fdd38703c4
vbaobao <vbao.main@gmail.com>
2021-03-08 00:01:56 -0800

    Minor changes to endpoints and positions

--- server/server.js
+++ server/server.js
@@ -5,11 +5,15 @@ const db = Promise.promisifiyAll(require('./database'));
 const PORT = 3000;
 
 app.use(express.static('./public'))
-app.listen(PORT);
 
 app.get('/api/:id', (req, res) => {
   console.log(req);
   db.getProductSizeAsync(req.param.id)
-    .then((res) => { return console.log(res); })
+    .then((res) => {
+      console.log(res);
+      res.send('OK');
+    })
     .catch();
 });
+
+app.listen(PORT, `Listening on http://localhost:${PORT}`);
