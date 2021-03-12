fa8c3ba5c0dc2226070a9488ab14e5a86a268315
vbaobao <vbao.main@gmail.com>
2021-03-12 03:02:52 -0800

    Debug callback parameters

--- server/database/database.js
+++ server/database/database.js
@@ -13,21 +13,21 @@ const db = {
   setProductSizes: (documents, callback) => {
     productSizes.insertMany(documents, (err, docs) => {
       if (err) { return callback(err); }
-      return callback(docs);
+      return callback(null, docs);
     });
   },
 
   getProductSizes: (callback) => {
     productSizes.find({}, (err, docs) => {
       if (err) { return callback(err); }
-      return callback(docs);
+      return callback(null, docs);
     });
   },
 
   getProductSize: (id, callback) => {
     productSizes.find({id: id}, (err, docs) => {
       if (err) { return callback(err); }
-      return callback(docs);
+      return callback(null, docs);
     });
   }
 };
