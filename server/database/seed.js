8024db6397b0f6155a602636f6cf972139a8ea8a
vbaobao <vbao.main@gmail.com>
2021-03-12 00:49:47 -0800

    Refactor to use promisifed db import

--- server/database/seed.js
+++ server/database/seed.js
@@ -1,22 +1,15 @@
-const Promise = require('bluebird');
 const mongoose = require('mongoose');
-
 const db = require('./database.js');
-const seed = Promise.promisify(db.setProductSizes);
-
 const models = require('./databaseModels.js');
-const productSizes = models.productSizes;
-
 const generateSeedData = require('./generateSeedData.js');
 
 
 /* Data Insertion */
-productSizes.deleteMany();
+models.productSizes.deleteMany();
 let data = generateSeedData();
-seed(data)
+db.setProductSizesAsync(data)
   .then((results) => {
     console.log(results);
   })
-  .catch(err => console.error(err));
-
-  mongoose.connection.close();
+  .catch(err => console.error(err))
+  .then(() => mongoose.disconnect());
