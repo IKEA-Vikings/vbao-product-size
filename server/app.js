2c9bde784771efeb600b6a0f2a94eeea8f38d90a
vbaobao <vbao.main@gmail.com>
2021-03-12 20:31:31 -0800

    Modularize app API calls

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const db = require('./database/database.js');
+
+app.use(express.static('./public'))
+
+app.get('/api/sizes/:id', (req, res) => {
+  db.getProductSizeAsync(req.params.id)
+    .then((result) => {
+      res.send(result[0].sizes);
+    })
+    .catch((err) => console.error(err));
+});
+
+module.exports = app;
