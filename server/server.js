646f4dd365616a33107a1eba36828d2735c5d43f
vbaobao <vbao.main@gmail.com>
2021-03-12 20:31:43 -0800

    Modularize server connection

--- server/server.js
+++ server/server.js
@@ -1,18 +1,4 @@
-const express = require('express');
-const app = express();
-const Promise = require('bluebird');
-const db = Promise.promisifyAll(require('./database'));
+const app = require('./app.js');
 const PORT = 3000;
-
-app.use(express.static('./public'))
-
-app.get('/api/sizes/:id', (req, res) => {
-  console.log(req.params.id);
-  db.getProductSizeAsync(req.params.id)
-    .then((result) => {
-      res.send(result[0].sizes);
-    })
-    .catch((err) => console.error(err));
-});
 
 app.listen(PORT, console.log(`Listening on http://localhost:${PORT}`));
