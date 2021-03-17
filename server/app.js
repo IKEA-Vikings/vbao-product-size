26e5fedbdc0091f8bc955ac78e45f2ff801c12c8
vbaobao <vbao.main@gmail.com>
2021-03-17 02:49:25 -0700

    Import cors from Express

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const db = require('./database/database.js');
 
 app.use(express.static('./public'))
+app.use(cors());
 
-app.get('/api/sizes/:id', (req, res) => {
+var corsOptions = {
+  origin: /localhost:300[0-4]$/,
+  method: 'GET'
+};
+app.get('/api/sizes/:id', cors(corsOptions), (req, res) => {
   db.getProductSizeAsync(req.params.id)
     .then((result) => {
       res.send(result);
