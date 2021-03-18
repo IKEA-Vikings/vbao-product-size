37c92d82fb9fcf51d9dc13e0d60ec1a99f9d216b
vbaobao <vbao.main@gmail.com>
2021-03-17 21:16:21 -0700

    Update seed script

--- server/database/generateSeedData.js
+++ server/database/generateSeedData.js
@@ -1,35 +1,71 @@
 const generateSeedData = () => {
   let data = [];
   const random = (max) => Math.floor(Math.random() * Math.floor(max));
-  const sizeType = [
-    ['length', 'in'],
-    ['width', 'in'],
-    ['height', 'in'],
-    ['Thread count', '/in^2'],
-    ['Filling weight', 'oz'],
-    ['Total weight', 'oz'],
-    ['Max. load', 'lb']
+  const sizeSets = [
+    [
+      ['Length', 'inch'],
+      ['Width', 'inch'],
+    ],
+    [
+      ['Length', 'inch'],
+      ['Width', 'inch'],
+      ['Thread count', 'inches squared'],
+      ['Filling weight', 'ounce'],
+      ['Total weight', 'ounce']
+    ],
+    [
+      ['Max. load', 'pound'],
+      ['Height', 'inch']
+    ]
   ];
+  let hasBonus = random(2);
 
   for (let id = 1; id <= 100; id++) {
     let document = {};
-    let numSizes = random(8);
+    let sizeType = random(2);
+    let sizingGroup = sizeSets[sizeType];
+    let numSizes = sizingGroup.length;
 
-    for (let j = 0; j < numSizes; j++) {
+    for (let j = 0; j < numSizes ; j++) {
       let temp = {};
-      let index = random(4);
-      //Pick random number
-      temp['name'] = sizeType[index][0];
+      temp['name'] = sizingGroup[j][0];
       //Add name and unit to temp
-      temp['unit'] = sizeType[index][1];
+      temp['unit'] = sizingGroup[j][1];
       //Pick random size, add to temp
-      temp['size'] = random(75);
+      temp['size'] = random(150);
       //Push temp to document[sizes];
       if (document.sizes === undefined) {
         document.sizes = [];
       }
       document.sizes.push(temp);
     }
+
+    // Bonus sizing if exists
+    if (hasBonus && document.sizes !== undefined) {
+      let sizeType = random(2);
+      let sizingGroup = sizeSet[3][sizeType];
+      let temp = {};
+      temp['name'] = sizingGroup[0];
+      //Add name and unit to temp
+      temp['unit'] = sizingGroup[1];
+      //Pick random size, add to temp
+      temp['size'] = random(250);
+      document.sizes.push(temp);
+    }
+
+    // Generate title
+    if (sizeType === 0) {
+      let length = document.sizes[0].size;
+      let width = document.sizes[1].size;
+      document.title = `${length}x${width}"`;
+    } else if (sizeType === 1) {
+      let sizes = ['Twin', 'Single', 'Queen', 'King']
+      let title = random(5);
+      document.title = `${sizes[title]}`;;
+    } else {
+      document.title = '';
+    }
+
     //push document to data
     document.id = id;
     data.push(document);
