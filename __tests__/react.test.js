f1d418c0c32cce84aeedad05219c10bd842c55d4
vbaobao <vbao.main@gmail.com>
2021-03-27 14:37:57 -0700

    Add ProductSize component to tests

--- __tests__/react.test.js
+++ __tests__/react.test.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import axios from 'axios';
 import {mount,shallow,render,configure} from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
+import ProductSize from '../client/ProductSize.jsx';
 import ProductSizeButton from '../client/ProductSizeButton.jsx';
 import ProductSizeModal from '../client/ProductSizeModal.jsx';
 
 configure({ adapter: new Adapter() });
+jest.mock('axios');
+
+describe('ProductSize Service Components', () => {
+  it('renders without crashing', () => {
+    const component = shallow(<ProductSize />);
+  });
+
+  it('should render correctly', () => {
+    const component = shallow(<ProductSize />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should toggleOverlay should update state', () => {
+    const component = mount(<ProductSize />);
+    component.find('.overlay').simulate('click');
+    expect(component.instance().state.overlay).toBe('overlay unhide');
+    component.find('.overlay').simulate('click');
+    expect(component.instance().state.overlay).toBe('overlay');
+  });
+
+  it('should update state after componentDidMount', () => {
+    const spy = jest.spyOn(axios, 'get');
+    expect(spy).toHaveBeenCalledTimes(0);
+
+    const component = mount(<ProductSize />);
+    expect(spy).toHaveBeenCalledTimes(2);
+  });
+});
 
 describe('ProductSizeButton', () => {
   it('renders without crashing', () => {
