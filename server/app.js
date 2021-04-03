86bc9ea38365d775ea6aaea66079080ce739f99d
vbaobao <vbao.main@gmail.com>
2021-04-02 19:53:41 -0700

    Handle DB errors

--- server/app.js
+++ server/app.js
@@ -17,7 +17,8 @@ app.get('/api/sizes/:id', (req, res) => {
   db.getProductSizeAsync(req.params.id)
     .then((result) => {
       res.send(result);
-    });
+    })
+    .catch((err) => console.error(err));
 });
 
 module.exports = app;
